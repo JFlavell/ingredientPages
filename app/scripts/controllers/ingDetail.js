774b2053a35aa587b6abb42087c48e45f3b13d53
Jonathan Flavell <jflavell@whisk.co.uk>
2013-10-23 22:15:26 +0100

    add/remove links, JSON tidy-up, show/hide links

--- app/scripts/controllers/ingDetail.js
+++ app/scripts/controllers/ingDetail.js
@@ -5,5 +5,16 @@ angular.module('IngredientPagesApp')
   	function($scope, $routeParams, $http) {
 			$http.get('ingredients/' + $routeParams.ingredient + '.json').success(function(data) {
   			$scope.ing = data;
+  			$scope.length = $scope.ing.ingredient.links.length;
+
+  			$scope.add = function(type, ingredient){
+					$scope.ing.ingredient.links.push({name:type, ingredient: ingredient});
+					$scope.length = $scope.ing.ingredient.links.length;
+				}
+
+				$scope.delete = function(index){
+					$scope.ing.ingredient.links.splice(index, 1);
+					$scope.length = $scope.ing.ingredient.links.length;
+				}
   		});
-  }]);
+	}]);
